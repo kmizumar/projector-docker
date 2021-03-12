7004df2a30bc01554e978119a3c24530b2a35da9
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2021-03-12 03:27:34 +0000

    change to use IntelliJ IDEA Ultimate 2020.3.2 by default

--- build-container-dev.sh
+++ build-container-dev.sh
@@ -24,8 +24,8 @@ cd ../projector-server
 ./gradlew :projector-server:distZip
 cd -
 
-containerName=${1:-projector-idea-c}
-downloadUrl=${2:-https://download.jetbrains.com/idea/ideaIC-2019.3.5.tar.gz}
+containerName=${1:-projector-idea-u}
+downloadUrl=${2:-https://download.jetbrains.com/idea/ideaIU-2020.3.2.tar.gz}
 
 # build container:
 DOCKER_BUILDKIT=1 docker build --progress=plain -t "$containerName" --build-arg buildGradle=false --build-arg "downloadUrl=$downloadUrl" -f Dockerfile ..
